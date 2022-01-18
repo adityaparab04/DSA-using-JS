0016292e6332e723e317b5cda8eee67f4de11e75
adityaparab04 <adityaparab04@gmail.com>
2022-01-18 12:11:23 -0500

    reverse string problem added

--- reverseString.js
+++ reverseString.js
@@ -1,10 +1,9 @@
 let str = 'aditya';
 
 let arrStr = str.split('');
-console.log(arrStr);
+
 let newArr = [];
 let j = 0;
-console.log(arrStr.length)
 for(let i = arrStr.length; i>0; i--){
     newArr[j] = arrStr[i-1];
     j++; 
